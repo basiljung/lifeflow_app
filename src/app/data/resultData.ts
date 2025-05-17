0d2ba065ea38961e3c4669c5c53903b727fea1f0
basiljung <basil.jung@hotmail.ch>
2025-05-17 21:44:14 +0200

    basic version

---
+++ src/app/data/resultData.ts
@@ -0,0 +1,92 @@
+export type Neurotransmitter =
+  | 'oxytocin'
+  | 'serotonin'
+  | 'dopamine'
+  | 'endorphins'
+  | 'sinn'
+  | 'machbarkeit'
+  | 'ziel';
+
+export const RESULTDATA: {
+  [key: string]: {
+    tips: { title: string; text: string }[];
+    reasons: string[];
+    input: string;
+  };
+} = {
+  dopamine: {
+    tips: [
+      {
+        title: '1. Dopamin-Detox-Zonen schaffen',
+        text: '👉 Lege bewusste Zeiten ohne Social Media, Handy & Reizüberflutung ein (z. B. 1–2 Stunden morgens ohne Bildschirm).→ Das reduziert künstliche Reize und hilft dem System, sich neu zu regulieren.',
+      },
+    ],
+    reasons: [
+      'Zu viel kurzfristige Belohnung (Handy, Zucker, Serien, etc.)',
+      'Low reward sensitivity',
+    ],
+    input:
+      'Dopamin sorgt dafür, dass du motiviert bist, etwas zu tun und Herausforderungen anzugehen. Es macht dich neugierig, fokussiert und zielgerichtet. Ohne ausreichend Dopamin fehlt dir der Antrieb – du schiebst Dinge auf, lässt dich leicht ablenken und suchst nach schnellen Belohnungen. Ein stabiles Dopamin-System hilft dir, dranzubleiben, deinen Weg zu gehen, und die Aufgaben und Ideen umzusetzen, die dir wichtig sind und dich erfüllen.',
+  },
+  oxytocin: {
+    tips: [
+      {
+        title: '1. Echte Nähe suchen',
+        text: '👉 → Umarme bewusst Menschen, halte Händchen, nimm Blickkontakt auf – auch kurze, ehrliche Berührung wirkt.',
+      },
+    ],
+    reasons: ['🧍‍♂️ Wenig echte Verbindung', '🔄 Vergleich in sozialen Medien'],
+    input:
+      'Oxytocin ist ein Hormon und Neurotransmitter, das für Vertrauen, Nähe und soziale Bindung sorgt. Es wird z. B. beim Kuscheln, Umarmen oder in tiefen Gesprächen ausgeschüttet und stärkt das Gefühl von Verbundenheit und Sicherheit.',
+  },
+  serotonin: {
+    tips: [
+      {
+        title: '1. ...',
+        text: '👉 ...',
+      },
+    ],
+    reasons: ['Mood instability', 'Lack of structure and sunlight'],
+    input: 'blabla',
+  },
+  endorphins: {
+    tips: [
+      {
+        title: '1. ...',
+        text: '👉 ....',
+      },
+    ],
+    reasons: ['Mood instability', 'Lack of structure and sunlight'],
+    input: 'blabla',
+  },
+  ziel: {
+    tips: [
+      {
+        title: '1. ....',
+        text: '👉 ',
+      },
+    ],
+    reasons: ['Mood instability', 'Lack of structure and sunlight'],
+    input: 'zielzielziele',
+  },
+  machbarkeit: {
+    tips: [
+      {
+        title: '1. ....',
+        text: '👉......u regulieren.',
+      },
+    ],
+    reasons: ['llll', 'l'],
+    input: 'machbarkeit',
+  },
+  sinn: {
+    tips: [
+      {
+        title: '1. ....',
+        text: '👉 L ohne Bildscm, sich neu zu regulieren.',
+      },
+    ],
+    reasons: ['Mood instability', 'Lack of structure and sunlight'],
+    input: 'sinn',
+  },
+};
--- src/app/neurotransmitter-quiz/neurotransmitter-quiz.component.ts
+++ src/app/neurotransmitter-quiz/neurotransmitter-quiz.component.ts
@@ -1,45 +1,90 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { QUESTIONS } from '../data/questions';
+import { RESULTDATA } from '../data/resultData';
+import { Neurotransmitter } from '../data/resultData';
+import { CommonModule } from '@angular/common';
 
-type Neurotransmitter = 'oxytocin' | 'serotonin' | 'dopamine' | 'endorphins';
+declare var M: any;
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   selector: 'app-neurotransmitter-quiz',
   templateUrl: './neurotransmitter-quiz.component.html',
   styleUrls: ['./neurotransmitter-quiz.component.scss'],
 })
-export class NeurotransmitterQuizComponent {
+export class NeurotransmitterQuizComponent implements AfterViewInit {
   currentQuestionIndex = 0;
 
   questions = QUESTIONS;
+  resultData = RESULTDATA;
+  showResult = false;
+  showQuestions = true;
+
+  ngAfterViewInit() {
+    M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
+  }
 
   scores: Record<Neurotransmitter, number> = {
     oxytocin: 0,
     serotonin: 0,
     dopamine: 0,
     endorphins: 0,
+    ziel: 0,
+    machbarkeit: 0,
+    sinn: 0,
   };
+  resultDataSet: {
+    [key: string]: {
+      tips: { title: string; text: string }[];
+      reasons: string[];
+    };
+  } = {};
 
   selectAnswer(effects: any) {
     for (let key in effects) {
       this.scores[key as Neurotransmitter] += effects[key];
     }
     this.currentQuestionIndex++;
+
+    if (this.isQuizComplete()) {
+      this.showQuestions = false;
+      this.showResult = true;
+      this.getTestResults();
+    }
   }
 
-  get isQuizComplete() {
+  isQuizComplete() {
     return this.currentQuestionIndex >= this.questions.length;
   }
 
-  get resultMessage() {
-    if (!this.isQuizComplete) return '';
+  getTestResults() {
     const results = [];
     for (let key in this.scores) {
       const score = this.scores[key as Neurotransmitter];
       if (score >= 3) {
-        results.push(`Possible low ${key}`);
+        results.push(key);
       }
     }
-    return results.length > 0 ? results.join(', ') : 'All systems balanced!';
+    this.resultDataSet = results.reduce(
+      (acc, key) => {
+        if (this.resultData[key]) {
+          acc[key] = this.resultData[key];
+        }
+        return acc;
+      },
+      {} as {
+        [key: string]: {
+          tips: { title: string; text: string }[];
+          reasons: string[];
+        };
+      },
+    );
+
+    setTimeout(() => {
+      const elems = document.querySelectorAll('.collapsible');
+      M.Collapsible.init(elems);
+    }, 0);
+    return this.resultDataSet;
   }
 }
