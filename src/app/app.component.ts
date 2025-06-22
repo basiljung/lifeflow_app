fe74eafc76ccb6cc31a55d11c3b919931ced532d
basiljung <basil.jung@hotmail.ch>
2025-06-22 11:38:44 +0200

    adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 
-declare var window: any;
-declare var cookieconsent: any;
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,6 +10,4 @@ declare var cookieconsent: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'neurotransmitter-quiz';
-}
+export class AppComponent {}
