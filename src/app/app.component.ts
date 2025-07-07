6d968094db58c7dc25526c4d04f01ae0917eb128
basiljung <basil.jung@hotmail.ch>
2025-07-07 17:29:02 +0200

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,4 +16,15 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-DFYEQXV8R9', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
+}
+declare function gtag(command: string, target: string, params?: any): void;
