b13c0f22695bc7872fd82d429f9893c64bec48d9
basiljung <basil.jung@hotmail.ch>
2025-08-06 18:14:40 +0200

    color adjustment

--- src/app/cta-email-course/cta-email-course.component.ts
+++ src/app/cta-email-course/cta-email-course.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { LanguageService } from '../language.service';
 import { Subject, takeUntil } from 'rxjs';
 
+declare function gtag(command: string, target: string, params?: any): void;
+
 @Component({
   selector: 'app-cta-email-course',
   imports: [],
@@ -11,6 +13,7 @@ import { Subject, takeUntil } from 'rxjs';
 export class CtaEmailCourseComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   currentLang: string | null = null;
+  isLoading = false;
 
   constructor(private langService: LanguageService) {}
 
@@ -18,6 +21,20 @@ export class CtaEmailCourseComponent implements OnInit, OnDestroy {
     this.langService.lang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
       this.currentLang = lang;
     });
+  }
+
+  openSignUpDialog() {
+    this.isLoading = true;
+    // 🔥 Trigger Google Analytics event
+    gtag('event', 'selfcheck_languageSelected', {
+      event_category: 'selfcheck',
+      event_label: 'Questions answered',
+      value: 1,
+    });
+    setTimeout(() => {
+      const elems = document.querySelectorAll('.collapsible');
+      this.isLoading = false;
+    }, 1500);
   }
 
   ngOnDestroy(): void {
