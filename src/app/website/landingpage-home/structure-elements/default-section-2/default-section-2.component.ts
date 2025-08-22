e293291557ff7e871c1672ddeef7892f6da20cdd
basiljung <basil.jung@hotmail.ch>
2025-08-22 21:11:57 +0200

    part 4

--- src/app/website/landingpage-home/structure-elements/default-section-2/default-section-2.component.ts
+++ src/app/website/landingpage-home/structure-elements/default-section-2/default-section-2.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-default-section-2',
   imports: [],
   templateUrl: './default-section-2.component.html',
-  styleUrl: './default-section-2.component.scss'
+  styleUrl: './default-section-2.component.scss',
 })
-export class DefaultSection2Component {
+export class DefaultSection2Component implements AfterViewInit {
+  activeStep = 1;
 
+  @ViewChild('rightPane', { static: false })
+  rightPane!: ElementRef<HTMLElement>;
+  @ViewChildren('storyItem') storyItems!: QueryList<ElementRef<HTMLElement>>;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    const io = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((e) => {
+          if (e.isIntersecting) {
+            const idx =
+              this.storyItems
+                .toArray()
+                .findIndex((ref) => ref.nativeElement === e.target) + 1;
+            if (idx > 0 && this.activeStep !== idx) {
+              this.activeStep = idx;
+              this.cdr.markForCheck();
+            }
+          }
+        });
+      },
+      {
+        // 👇 this is the important change
+        root: this.rightPane.nativeElement,
+        threshold: 0.15,
+        rootMargin: '0px 0px -15% 0px',
+      },
+    );
+
+    this.storyItems.forEach((ref) => io.observe(ref.nativeElement));
+  }
 }
