1fb91717efe29bd82bddf2b41f3122eb7dbd8a2c
basiljung <basil.jung@hotmail.ch>
2025-08-22 14:22:36 +0200

    optimizing first part

--- src/app/website/get-details-btn/get-details-btn.component.ts
+++ src/app/website/get-details-btn/get-details-btn.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { LanguageService } from '../../services/language.service';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -12,13 +12,35 @@ import { Subject, takeUntil } from 'rxjs';
 export class GetDetailsBtnComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   currentLang: string | null = null;
+  topic: string | undefined;
 
-  constructor(private langService: LanguageService) {}
+  constructor(
+    private langService: LanguageService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+    const segments = this.router.url.split('/').filter(Boolean);
+
+    this.topic = segments[1];
     this.langService.lang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
       this.currentLang = lang;
     });
+  }
+
+  scrollTo(id: string) {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  handleAction() {
+    if (this.topic) {
+      this.scrollTo('result-selector');
+    } else {
+      this.scrollTo('topic-selector');
+    }
   }
 
   ngOnDestroy(): void {
