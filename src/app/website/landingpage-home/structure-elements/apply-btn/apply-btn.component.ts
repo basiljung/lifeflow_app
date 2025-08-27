602828b6ba7b05759c16293db971f594b15eeb66
basiljung <basil.jung@hotmail.ch>
2025-08-27 12:21:36 +0200

    apply btn

--- src/app/website/landingpage-home/structure-elements/apply-btn/apply-btn.component.ts
+++ src/app/website/landingpage-home/structure-elements/apply-btn/apply-btn.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { LanguageService } from '../../../../services/language.service';
 
+declare function gtag(command: string, target: string, params?: any): void;
+
 @Component({
   selector: 'app-apply-btn',
   imports: [],
@@ -11,6 +13,7 @@ import { LanguageService } from '../../../../services/language.service';
 export class ApplyBtnComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   currentLang: string | null = null;
+  isLoading = false;
 
   constructor(private langService: LanguageService) {}
 
@@ -18,6 +21,21 @@ export class ApplyBtnComponent implements OnInit, OnDestroy {
     this.langService.lang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
       this.currentLang = lang;
     });
+  }
+
+  applyCoachingDialog() {
+    this.isLoading = true;
+
+    // 🔥 Trigger Google Analytics event
+    gtag('event', 'apply_coaching_btn_clicked', {
+      event_category: 'apply_coaching',
+      event_label: 'btn_clicked',
+      value: 1,
+    });
+    setTimeout(() => {
+      const elems = document.querySelectorAll('.collapsible');
+      this.isLoading = false;
+    }, 1500);
   }
 
   ngOnDestroy(): void {
