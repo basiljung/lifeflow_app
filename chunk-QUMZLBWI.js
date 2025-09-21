116db0181a72d00209fb5d51a22a7d4f35a13abb
basiljung <basil.jung@hotmail.ch>
2025-09-21 16:06:23 +0200

    Auto-generated commit

--- chunk-QUMZLBWI.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3HS32FA7.js";import"./chunk-46HYHXQE.js";import"./chunk-DXRMEWUO.js";import"./chunk-FAIG6ZOG.js";import"./chunk-M66ZQN2Y.js";export{a as LandingpageHomeComponent};
