d3e75dc8df2c6a637a108cb684a1b37fecbaa0ee
basiljung <basil.jung@hotmail.ch>
2025-09-21 16:09:37 +0200

    Auto-generated commit

---
+++ chunk-HXGWYHSF.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-5TZX2C4R.js";import"./chunk-46HYHXQE.js";import"./chunk-DXRMEWUO.js";import"./chunk-FAIG6ZOG.js";import"./chunk-M66ZQN2Y.js";export{a as LandingpageHomeComponent};
