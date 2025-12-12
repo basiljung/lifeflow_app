2fdbf3e3151007f3c9e89f4720983638c6f62981
basiljung <basil.jung@hotmail.ch>
2025-12-12 21:43:42 +0100

    email newsletter

--- src/app/general-elements/email-course-signup-page/email-course-signup-page.component.ts
+++ src/app/general-elements/email-course-signup-page/email-course-signup-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CtaEmailCourseComponent } from '../cta-email-course/cta-email-course.component';
+import { Subject, takeUntil } from 'rxjs';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-email-course-signup-page',
@@ -7,4 +9,21 @@ import { CtaEmailCourseComponent } from '../cta-email-course/cta-email-course.co
   templateUrl: './email-course-signup-page.component.html',
   styleUrl: './email-course-signup-page.component.scss',
 })
-export class EmailCourseSignupPageComponent {}
+export class EmailCourseSignupPageComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  currentLang: string | null = null;
+  isLoading = false;
+
+  constructor(private langService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.langService.lang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
+      this.currentLang = lang;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
